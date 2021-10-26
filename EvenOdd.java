ce843183830139195ab34ed78f9df4ab91def7c0
anilkumaranuu <92967432+anilkumaranuu@users.noreply.github.com>
2021-10-26 09:11:06 +0530

    java classes

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+class EvenOdd{
+        public static void main(String []nik){
+		
+		for (int index=0; index< nik.length;index++){
+		    System.out.print(number +"\t");
+			String number=nik[index];
+		int num=Integer.parseInt(number);
+		 System.out.println(num);
+	}
+ }
+}
